5ab53115097705100dab0c36921bd0e0641c17a8
sunny <sunnysarrot@gmail.com>
2015-07-12 13:41:19 +0530

    first commit

---
+++ app/controllers/spree/admin/roles_controller.rb
@@ -0,0 +1,37 @@
+module Spree
+	module Admin
+    class RolesController < ResourceController
+      before_filter :load_permissions, :only => [:edit, :new, :create, :update]
+      before_filter :restrict_unless_editable, :only => [:edit, :update]
+
+      def index
+        @roles = Spree::Role.page(params[:page])
+      end
+
+      def destroy
+        @role = Spree::Role.find(params[:id])
+        @role.destroy
+
+        flash[:success] = Spree.t('notice_messages.role_deleted')
+
+        respond_with(@role) do |format|
+          format.html { redirect_to collection_url }
+          format.js  { render_js_for_destroy }
+        end
+      end
+
+      private
+        def permitted_resource_params
+          params.require(:role).permit(:name, :permission_ids => [])
+        end
+
+        def load_permissions
+          @permissions = Spree::Permission.visible.all
+        end
+
+        def restrict_unless_editable
+          redirect_to admin_roles_path unless @role.editable?
+        end
+    end
+  end
+end
---
+++ app/models/spree/role_decorator.rb
@@ -0,0 +1,19 @@
+Spree::Role.class_eval do
+  
+  has_and_belongs_to_many :permissions, :join_table => 'spree_roles_permissions', :class_name => 'Spree::Permission'
+
+  validates :name, :presence => true, :uniqueness => true
+  preference :discount_percentage, :integer, default: '0'
+  
+  def ability(current_ability, user)
+    permissions.each do |permission|
+      permission.ability(current_ability, user)
+    end
+  end
+
+  def has_permission?(permission_title)
+    permissions.pluck(:title).include?(permission_title)
+  end
+
+  scope :default_role, lambda { where(:is_default => true) } 
+end
